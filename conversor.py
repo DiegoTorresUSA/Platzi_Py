395dcd0ce00e2a0962a0169503113b677019fdb8
dtorres <diego.torres.mt@correo.usa.edu.co>
2022-04-20 22:30:24 -0500

    learning some Python

--- conversor.py
+++ conversor.py
@@ -1,7 +1,32 @@
-pesos = input("Cúantos dinero quieres convertir: ")
-pesos = float(pesos)
-valor_dolar = 3875
-dolares = pesos / valor_dolar
-dolares = round(dolares,2)
-dolares = str(dolares)
-print("tienes $ " + dolares + "dólares")
+menu = """
+    Bienvenido al conversor de monedas 🪙
+    
+    1 - a Pesos colombianos
+    2 - a Pesos argentinos
+    3 - a Pesos Mexicanos
+    
+    Elige una opción: 
+"""
+
+opcion = int(input(menu))
+
+if opcion == 1:
+    pesos = input("Cúantos dinero quieres convertir: ")
+    pesos = float(pesos)
+    valor_dolar = 3875
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("tienes $ " + dolares + "dólares")
+elif opcion == 2:
+    pesos = input("Cúantos dinero quieres convertir: ")
+    pesos = float(pesos)
+    valor_dolar = 65
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("tienes $ " + dolares + "dólares")
+elif opcion == 3:
+    pass
+else:
+    print("Digita una opcion valida")
---
+++ funciones.py
@@ -0,0 +1,7 @@
+def imprimir_mensaje():
+    print("mensaje especial")
+    print("Aprendiendo a usar funciones")
+
+
+imprimir_mensaje()
+imprimir_mensaje()
